0a843c0e89b409f208e59460ce5375504b520f25
Sma-yida <45259710+Sma-yida@users.noreply.github.com>
2018-12-20 21:55:02 +0800

    DSR路由回复选项
    在dsr_rrep.h头文件中定义了路由回复选项，具体代码分析

--- dsr-uu-0.2/dsr-rrep.h
+++ dsr-uu-0.2/dsr-rrep.h
@@ -13,21 +13,24 @@
 
 #ifndef NO_GLOBALS
 
-struct dsr_rrep_opt {
-	u_int8_t type;
-	u_int8_t length;
-#if defined(__LITTLE_ENDIAN_BITFIELD)
-	u_int8_t res:7;
-	u_int8_t l:1;
-#elif defined (__BIG_ENDIAN_BITFIELD)
-	u_int8_t l:1;
-	u_int8_t res:7;
+struct dsr_rrep_opt { //DSR路由回复选项
+	u_int8_t type;  //选项类型
+	u_int8_t length;//选项长度
+#if defined(__LITTLE_ENDIAN_BITFIELD) //小端情况
+	u_int8_t res:7;              //保留位
+	u_int8_t l:1;                //置1：路由回复中的最后一跳到达了DSR网络的外部
+#elif defined (__BIG_ENDIAN_BITFIELD)//大端情况
+	u_int8_t l:1;                //置1：路由回复中的最后一跳到达了DSR网络的外部
+	u_int8_t res:7;              //保留位
 #else
 #error  "Please fix <asm/byteorder.h>"
 #endif
-	u_int32_t addrs[0];
+	u_int32_t addrs[0];        //记录路由恢复信息
 };
-
+/*代码第21行，l标志位代表Last Hop，置0表示最后一跳节点在DSR网络内部，置1表示最后一条到达了DSR网络的外部。
+在选取路由时应当遵循以下原则：尽量选取仅利用本网络内部节点就可以到达目的节点的路由，尽量不经过外部网络。
+即，在路由缓存中优先选择l标志位置0的路由。
+*/
 #define DSR_RREP_HDR_LEN sizeof(struct dsr_rrep_opt)
 #define DSR_RREP_OPT_LEN(srt) (DSR_RREP_HDR_LEN + srt->laddrs + sizeof(struct in_addr))
 /* Length of source route is length of option, minus reserved/flags field minus
