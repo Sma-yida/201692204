0c87d1b0ea6512c6a5d658658073c01156b7c1b0
Sma-yida <45259710+Sma-yida@users.noreply.github.com>
2018-12-21 09:58:42 +0800

    DSR路由错误选项
    在dsr_rerr.h头文件中定义了路由错误选项，具体代码分析

--- dsr-uu-0.2/dsr-rerr.h
+++ dsr-uu-0.2/dsr-rerr.h
@@ -16,22 +16,22 @@
 
 #ifndef NO_GLOBALS
 
-struct dsr_rerr_opt {
+struct dsr_rerr_opt {    //DSR路由错误选项
 	u_int8_t type;
 	u_int8_t length;
-	u_int8_t err_type;
+	u_int8_t err_type;   //错误类型
 #if defined(__LITTLE_ENDIAN_BITFIELD)
-	u_int8_t res:4;
-	u_int8_t salv:4;
+	u_int8_t res:4;      //保留位
+	u_int8_t salv:4;     //salvage:该数据包被重发的次数
 #elif defined (__BIG_ENDIAN_BITFIELD)
 	u_int8_t res:4;
 	u_int8_t salv:4;
 #else
 #error  "Please fix <asm/byteorder.h>"
 #endif
-	u_int32_t err_src;
-	u_int32_t err_dst;
-	char info[0];
+	u_int32_t err_src;   //路由错误的源地址
+	u_int32_t err_dst;   //路由错误目的地址
+	char info[0];        //记录路由错误信息
 };
 
 #define DSR_RERR_HDR_LEN sizeof(struct dsr_rerr_opt)
@@ -55,3 +55,4 @@ int dsr_rerr_opt_recv(struct dsr_pkt *dp, struct dsr_rerr_opt *dsr_rerr_opt);
 #endif				/* NO_DECLS */
 
 #endif				/* _DSR_RERR_H */
+//在dsr_rerr.h头文件中定义了路由错误选项
